d5bb5d0986f5fb6f2c3404ea149ef756c4c471c8
gabrielariver <riv23011@byui.edu>
2025-05-16 18:26:02 -0500

    Completed W02: Dynamic Product List

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+console.log("⚡ main.js is running");
+
+const dataSource = new ProductData("tents"); 
+const element = document.querySelector(".product-list"); 
+const productList = new ProductList("Tents", dataSource, element);
+
+productList.init();
