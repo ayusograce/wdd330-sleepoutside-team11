f6355ccb6caaa85a30501cb2d339df5c565c9bf4
gabrielariver <riv23011@byui.edu>
2025-05-21 16:51:54 -0500

    W03 Individual Activity: Expand Inventory

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,26 +1,41 @@
-import { updateCartNum, loadHeaderFooter } from "./utils.mjs";
-import { renderCartContents} from "./ShoppingCart.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
-//get the products from the cart
-const cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+loadHeaderFooter();
+
+const cart = JSON.parse(localStorage.getItem('so-cart')) || [];
 
 if (cart.length > 0) {
-  document.querySelector(".cart-footer").classList.remove("hide");
+  document.querySelector('.cart-footer').classList.remove('hide');
 }
 
 // Calculate the total by adding the price of each product
 const total = cart.reduce((sum, item) => sum + item.FinalPrice, 0);
 // Insert total
-document.querySelector(".cart-total").textContent =
-  `Total: $${total.toFixed(2)}`;
+document.querySelector('.cart-total').textContent = `Total: $${total.toFixed(2)}`;
 
+function renderCartContents() {
+  const cartItems = getLocalStorage("so-cart");
+  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+}
 
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Images?.PrimarySmall || 'images/fallback.jpg'}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${item.FinalPrice}</p>
+</li>`;
+
+  return newItem;
+}
 
 renderCartContents();
-
-
-//The number of items in the cart (header)
-updateCartNum();
-
-// To call the header and footer partials
-loadHeaderFooter();
