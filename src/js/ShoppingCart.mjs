669e6d844cdeb47b935f4690cd717f64da099db0
Grace Ayuso <adriayusof@gmail.com>
2025-05-23 03:15:22 -0500

    individual card: remove product button. Fix css en main page and butt… (#22)
    * individual card: remove product button. Fix css en main page and buttons in product pages.
    
    * new file to manage deploy error
    
    * changed path of partials

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage} from "./utils.mjs";
+import { getLocalStorage, updateCartNum} from "./utils.mjs";
 
 export function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -10,7 +10,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -20,7 +20,54 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="remove" aria-label="Delete product" data-id="${item.Id}">×</button>
 </li>`;
 
   return newItem;
+}
+
+// ga--function to display the result and show a message when the cart is empty
+// this code was in the cart.js, but it is better if it goes here and is imported there
+// I haven't deleted the code in the other file because that was not my card to do, but it
+// is a suggestion that I make (delete that other code and export and import this function)
+function totalOrEmpty(){
+  const cart = getLocalStorage("so-cart") || [];
+  if (cart.length > 0) {
+    document.querySelector('.cart-footer').classList.remove('hide');
+    renderCartContents();
+
+      // Calculate and display the total cart value
+    const total = cart.reduce((sum, item) => sum + item.FinalPrice, 0);
+    document.querySelector('.cart-total').textContent = `Total: $${total.toFixed(2)}`;
+  } else {
+
+      // If the cart is empty, show message
+    const emptyMessage = document.querySelector("#empty-message");
+    if (emptyMessage) {
+      emptyMessage.classList.remove("hide");
+      document.querySelector('.cart-total').textContent = `Total: $0`;
+    }
+  }
+}
+
+
+// ga--Function to eliminate products from the cart
+export function deleteProduct(){
+  const productsList = document.querySelector(".product-list");
+  if (!productsList)
+    return;
+
+  productsList.addEventListener("click", function(e){
+    if(e.target.classList.contains("remove")){
+      const product = e.target.closest("li");
+      const productName = product.querySelector(".card__name").textContent;
+
+      let cart = JSON.parse(localStorage.getItem("so-cart")) || [];
+      cart = cart.filter((item) => item.Name !== productName);
+      localStorage.setItem("so-cart", JSON.stringify(cart));
+        renderCartContents();
+        updateCartNum();
+        totalOrEmpty();
+      }
+    });
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,15 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter, updateCartNum } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-
-loadHeaderFooter();
 
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
 const product = new ProductDetails(productID, dataSource);
 product.init();
+
+//ga--The number of items in the cart (header)
+updateCartNum();
+
+// ga--To call the header and footer partials
+loadHeaderFooter();
