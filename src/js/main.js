f55ef48bbff3f1e8bb6557fa23f3932b7b4a7d5b
iriebijudicael <iriejudicaelelvis225@gmail.com>
2025-06-17 01:27:43 +0000

    Merge pull #30 from ayusograce/e-h-espada-newsletter-sign-up

--- src/js/main.js
+++ src/js/main.js
@@ -14,25 +14,25 @@ updateCartNum();
 // To call the header and footer partials
 loadHeaderFooter();
 
-document.addEventListener('DOMContentLoaded', () => {
-  const modal = document.getElementById('registerModal');
-  const closeButton = modal.querySelector('.close-button');
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("registerModal");
+  const closeButton = modal.querySelector(".close-button");
 
-  if (!localStorage.getItem('hasSeenRegisterModal')) {
-    modal.classList.remove('hidden');
-    localStorage.setItem('hasSeenRegisterModal', 'true');
+  if (!localStorage.getItem("hasSeenRegisterModal")) {
+    modal.classList.remove("hidden");
+    localStorage.setItem("hasSeenRegisterModal", "true");
   }
 
-  closeButton.addEventListener('click', () => {
-    modal.classList.add('hidden');
+  closeButton.addEventListener("click", () => {
+    modal.classList.add("hidden");
   });
 
   // Newsletter form handling
-  const newsletterForm = document.getElementById('newsletter-form');
+  const newsletterForm = document.getElementById("newsletter-form");
   if (newsletterForm) {
-    newsletterForm.addEventListener('submit', function(event) {
+    newsletterForm.addEventListener("submit", function(event) {
       event.preventDefault();
-      const emailInput = document.getElementById('email');
+      const emailInput = document.getElementById("email");
       const email = emailInput.value.trim();
 
       if(email === ""){
